673db6dfb01db3bc5b9741b60f229d9cb7be135f
Aryan Patel <patelaryan7751@gmail.com>
2024-05-13 21:06:31 +0530

    Final changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let todos = [];
 
+// eslint-disable-next-line no-unused-vars
 function addTodo() {
     const input = document.getElementById('todo-input');
     if (input.value.trim() !== '') {
@@ -9,12 +10,28 @@ function addTodo() {
     }
 }
 
+function removeTodo(index) {
+    todos.splice(index, 1); // Remove the todo at the specified index
+    renderTodos(); // Re-render the todo list
+}
+
 function renderTodos() {
     const list = document.getElementById('todo-list');
     list.innerHTML = '';
     todos.forEach((todo, index) => {
         const listItem = document.createElement('li');
         listItem.textContent = todo;
+
+        // Create a remove button for each todo
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Delete';
+        removeBtn.style.marginLeft = '10px';
+        removeBtn.style.color = 'red';
+        removeBtn.onclick = function () {
+            removeTodo(index);
+        }; // Attach click handler to remove todo
+        listItem.appendChild(removeBtn);
+
         list.appendChild(listItem);
     });
 }
